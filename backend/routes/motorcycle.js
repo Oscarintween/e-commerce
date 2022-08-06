cbe77fb9444dc95280aae952359e4f9062cafec0
Oscarintween <oscarintween@gmail.com>
2022-08-05 19:41:44 -0500

    Delete backend directory

--- backend/routes/motorcycle.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const {getMotorcycles,getOneMotorcycle} = require('../controllers/motorcycles')
-
-router.get('/', getMotorcycles)
-router.get('/:id',getOneMotorcycle)
-
-module.exports = router
